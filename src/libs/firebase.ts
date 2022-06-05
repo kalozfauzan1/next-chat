292b63b6412ff000be38484538e60ef753fa78fa
kaloz fauzan <kaloz.fauzan2@gmail.com>
2022-06-06 01:47:25 +0700

    init

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -3,10 +3,10 @@ import { getAuth } from 'firebase/auth';
 import { config } from '@/libs/config';
 
 const firebaseConfig = {
-  apiKey: config.firebaseApiKey,
-  authDomain: config.firebaseAuthDomain,
-  projectId: config.firebaseProjectId,
-  appId: config.firebaseAppId,
+  apiKey: `AIzaSyBynE89w2fZo9zat2EF-erHzJCBviz0YeQ`,
+  authDomain: `firts-project-119b8.firebaseapp.com`,
+  projectId: `firts-project-119b8`,
+  appId: `1:109104554165:web:b4e75996dc9f0957809efc`,
 };
 
 if (!getApps().length) {
